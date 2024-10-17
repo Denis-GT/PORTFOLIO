643cf3561381899f7c44ff4603d5aa9c6b69c105
Denis GATEV <denis.gatev@lycee-cci-gard.fr>
2024-10-17 17:59:51 +0200

    int

---
+++ assets/projets/Maison Culture/js/script.js
@@ -0,0 +1,11 @@
+function calcul(){
+
+var cat = document.getElementById('categorie').value;
+
+ var NbJ = document.getElementById('nobre_jours').value;
+
+resultat = NbJ  + cat;
+
+
+tarif.value = resultat *1.5;
+}
